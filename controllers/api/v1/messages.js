24e6899335880365d72f0b321a6a58329f550906
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-11-21 17:17:13 +0100

    feat: PUT en DEL functions

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -31,5 +31,59 @@ const create = async (req, res) => {
     });
 };
 
+// making a put request
+const update = async (req, res) => {
+    try {
+        let id = req.params.id;
+        let newData = req.body; // Assuming you're sending updated data in the request body
+        let message = await Message.findById(id);
+
+        if (!message) {
+            return res.status(404).json({
+                status: "error",
+                message: "Message not found"
+            });
+        }
+
+        // Update the message object with the new data
+        message = Object.assign(message, newData);
+
+        // Save the updated message
+        await message.save();
+
+        res.json({
+            status: "success",
+            message: "PUT completed"
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: "error",
+            message: "Internal server error"
+        });
+    }
+};
+
+// making an delete request
+const destroy = async (req, res) => {
+    let id = req.params.id;
+    let m = await Message.findOneAndDelete({
+        _id: id,
+    });
+
+    res.json({
+        status: "success",
+        message: "DELETE a message",
+        data: [
+            {
+                message: m,
+            },
+        ],
+    });
+};
+
+
 module.exports.index = index;
 module.exports.create = create;
+module.exports.update = update;
+module.exports.destroy = destroy;
